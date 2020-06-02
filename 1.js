2c7f011037632e843d111453a3c03e598acd0961
arpitk02 <akshkumar852@gmail.com>
2020-06-02 09:34:45 +0530

    update index

--- 1.js
+++ 1.js
@@ -9,3 +9,39 @@ function closeNav()
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginRight= "0";
 }
+
+
+function slideshow() {
+  // clone
+  $('.slider-1').clone().removeClass('slider-1').addClass('slider-2').insertAfter($('.slider'));
+
+  // set first
+  $('.slider-1').slick({
+    draggable: false,
+    dots: false,
+    infinite: true,
+    responsive: true,
+    asNavFor: '.slider-2',
+    touchThreshold: 20,
+    speed: 1000,
+    fade: true
+  });
+
+  // set second
+  $('.slider-2').slick({
+    dots: true,
+    infinite: true,
+    responsive: true,
+    asNavFor: '.slider-1',
+    arrows: false,
+    speed: 1000,
+    easing: 'easeInOutQuart'
+  });
+}
+
+$(function() {
+  slideshow();
+  setTimeout(function() {
+    $('.slider-1 .slick-next').click();
+  }, 1000);
+})
